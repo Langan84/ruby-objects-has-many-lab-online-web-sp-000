f1946d4969375016e44505d46a8495f6c09e339a
langan.kg Langan <langan.kg@gmail.com>
2021-06-11 17:04:12 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,10 +2,11 @@ class Song
 
   attr_accessor :artist, :name, :genre
 
-  @@all = []
 
-  def initialize(name)
+
+  def initialize(name, genre)
   @name = name
+  @genre = genre
   end
 
   #def save
